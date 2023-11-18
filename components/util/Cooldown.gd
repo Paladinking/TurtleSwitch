7c94bf47fa0c14b729b2d959935cac7be232f7ac
Empa <emanuel@empa.xyz>
2023-11-18 11:58:40 +0100

    Dashes

--- components/util/Cooldown.gd
+++ components/util/Cooldown.gd
@@ -5,29 +5,24 @@ extends Node
 @export_range(0., 100.)
 var cooldown: float = 1.
 
-var _on_cooldown: bool = false
 var _time_left: float = 0.
 
 signal on_cooldown_tick(left: float, cooldown: float)
-signal on_cooldown_changed(on_cooldown: bool)
+signal on_cooldown_ended
 
 func _physics_process(delta):
-    if self._time_left > 0.:
-        self._time_left = max(self._time_left - delta, 0.)
+	if _time_left > 0.:
+		_time_left = max(_time_left - delta, 0.)
 
-        if self._time_left == 0.:
-            self.on_cooldown_changed.emit(false)
-            self._on_cooldown = false
+		if _time_left == 0.:
+			on_cooldown_ended.emit()
 
-    self.on_cooldown_tick.emit(self._time_left, self.cooldown)
+	on_cooldown_tick.emit(_time_left, cooldown)
 
 func use_cooldown() -> bool:
-    if self._on_cooldown:
-        return false
+	if _time_left > 0.:
+		return false
 
-    self._on_cooldown = true
+	_time_left = cooldown
 
-    self.on_cooldown_changed.emit(true)
-    self._time_left = self.cooldown
-
-    return true
+	return true
