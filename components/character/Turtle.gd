48501b315d38a473b8ab76f122520351345185b0
Empa <emanuel@empa.xyz>
2023-11-18 14:24:22 +0100

    Shell pickups

--- components/character/Turtle.gd
+++ components/character/Turtle.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-
 const SPEED = 3.0
 const DASH_SPEED = 7.
 const DASH_TIME = 0.1
@@ -17,8 +16,16 @@ var _is_dashing: bool = false
 var _dash_cooldown: Cooldown
 var _dash_direction: Vector3
 
+
 func _ready():
 	_dash_cooldown = $DashCooldown
+	$PickupDetector.area_entered.connect(
+		func(area: Area3D):
+			print(area)
+			if area is ShellPickup:
+				pick_up(area.shell_type)
+	)
+
 
 func set_input(index):
 	left_input = "p" + str(index) + "_left"
@@ -27,6 +34,7 @@ func set_input(index):
 	right_input = "p" + str(index) + "_right"
 	action_input = "p" + str(index) + "_action"
 
+
 func _physics_process(delta):
 	# Gravity
 	if not is_on_floor():
@@ -34,7 +42,9 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed(action_input) and _dash_cooldown.use_cooldown():
 		_is_dashing = true
-		_dash_direction = -Vector3(cos(-rotation.y - PI / 2), 0., sin(-rotation.y - PI / 2)).normalized()
+		_dash_direction = -(
+			Vector3(cos(-rotation.y - PI / 2), 0., sin(-rotation.y - PI / 2)).normalized()
+		)
 		get_tree().create_timer(DASH_TIME, true, true).timeout.connect(func(): _is_dashing = false)
 
 	if _is_dashing:
@@ -44,7 +54,7 @@ func _physics_process(delta):
 		move_and_slide()
 	else:
 		var input_dir = Input.get_vector(left_input, right_input, up_input, down_input)
-	
+
 		if input_dir:
 			var angle = -atan2(input_dir.y, input_dir.x) + PI / 2
 			rotation.y = angle
@@ -55,3 +65,6 @@ func _physics_process(delta):
 			velocity.z = move_toward(velocity.z, 0, SPEED)
 
 		move_and_slide()
+
+func pick_up(shell: Shell.Kind):
+	print("pick up shell ", Shell.Kind.keys()[shell])
---
+++ components/shells/Shell.gd
@@ -0,0 +1,6 @@
+class_name Shell
+
+enum Kind {
+	NONE,
+	BASIC,
+}
