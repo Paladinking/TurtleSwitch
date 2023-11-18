34598ea75262ea67300f7f79256bf1af050f81d4
Axel <axel.hammarberg@gmail.com>
2023-11-18 10:49:24 +0100

    Added per player input

--- components/character/Turtle.gd
+++ components/character/Turtle.gd
@@ -6,7 +6,16 @@ const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var left = "none"
+var right = "none"
+var up = "none"
+var down = "none"
 
+func set_input(index):
+	left = "p" + str(index) + "_left"
+	up = "p" + str(index) + "_up"
+	down = "p" + str(index) + "_down"
+	right = "p" + str(index) + "_right"
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,7 +28,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector(left, right, up, down)
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
--- components/scenes/Main.gd
+++ components/scenes/Main.gd
@@ -1,5 +1,50 @@
 extends Node3D
 
+const INPUT_NAMES = [
+	"left", "right", "up", "down"
+]
+
+const CONTROLLER_INPUTS = [
+	[JOY_BUTTON_DPAD_LEFT, JOY_AXIS_LEFT_X, -1.0],
+	[JOY_BUTTON_DPAD_RIGHT, JOY_AXIS_LEFT_X, 1.0],
+	[JOY_BUTTON_DPAD_UP, JOY_AXIS_LEFT_Y, -1.0],
+	[JOY_BUTTON_DPAD_DOWN, JOY_AXIS_LEFT_Y, 1.0]
+]
+
+const KEYBOARD_INPUTS = [
+	[KEY_LEFT, KEY_A],
+	[KEY_RIGHT, KEY_D],
+	[KEY_UP, KEY_W],
+	[KEY_DOWN, KEY_S]
+]
+
+func _ready():
+	var joypads = Input.get_connected_joypads()
+	
+	var players = get_meta("Players")
+	for index in players.size():
+		var player = get_node(players[index])
+		for i in INPUT_NAMES.size():
+			var name = "p" + str(index) + "_" + INPUT_NAMES[i]
+			InputMap.add_action(name)
+			if index < joypads.size():
+				var ev1 = InputEventJoypadButton.new()
+				ev1.button_index = CONTROLLER_INPUTS[i][0]
+				ev1.device = joypads[index]
+				InputMap.action_add_event(name, ev1)
+				var ev2 = InputEventJoypadMotion.new()
+				ev2.axis = CONTROLLER_INPUTS[i][1]
+				ev2.axis_value = CONTROLLER_INPUTS[i][2]
+				ev2.device = joypads[index]
+				InputMap.action_add_event(name, ev2)
+			elif index == joypads.size():
+				for key in KEYBOARD_INPUTS[i]:
+					var ev = InputEventKey.new()
+					ev.keycode = key
+					InputMap.action_add_event(name, ev)
+
+		player.set_input(index)
+
 
 func _unhandled_key_input(event):
 	if event.is_pressed() and event.keycode == KEY_ESCAPE:
