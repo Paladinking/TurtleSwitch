450dacad7f29dc7beb7a7702201ced8f62b973b6
Axel <axel.hammarberg@gmail.com>
2023-11-19 10:27:49 +0100

    Better collisions

--- components/shells/Shell.gd
+++ components/shells/Shell.gd
@@ -45,3 +45,25 @@ func update_shell_visibility():
 			$ShellBasic.show()
 		Kind.POWER:
 			$PowerShell.show()
+
+static func get_power(shell: Shell, is_dashing: bool):
+	var kind
+	if shell == null:
+		kind = Kind.NONE
+	else:
+		kind = shell.kind
+	if is_dashing:
+		match kind:
+			Kind.NONE:
+				return 30
+			Kind.BASIC:
+				return 30
+			Kind.POWER:
+				return 100
+	match kind:
+		Kind.NONE:
+			return 3
+		Kind.BASIC:
+			return 5
+		Kind.POWER:
+			return 10
