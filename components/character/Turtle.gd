8142962790bafeb0e245839c6f846820900aa1d5
Axel <axel.hammarberg@gmail.com>
2023-11-19 11:46:59 +0100

    Bash shells + more shell logic

--- components/character/Turtle.gd
+++ components/character/Turtle.gd
@@ -1,12 +1,11 @@
 class_name Turtle
 extends CharacterBody3D
 
-const ACCELERATION = 3.0
 const COLLISION_DURATION = 0.2
 const MAX_VELOCITY = 11.0
 const FRICTION = 0.04
-const DASH_SPEED = 14.0
 const DASH_TIME = 0.2
+const BASH_TIME = 1.2
 const MAX_ROTATION = PI / 16
 const MIN_SPEED_FOR_ANIMATION = 5
 
@@ -19,9 +18,10 @@ var down_input: String
 var action_input: String
 
 var _collision_acceleration = Vector3(0, 0, 0)
-var _is_dashing: bool = false
 var _dash_cooldown: Cooldown
-var _dash_direction: Vector3
+var _bash_cooldown: Cooldown
+var _bash_area: Area3D
+var _action_direction: Vector3
 var _shell: Shell = null:
 	set(new_shell):
 		if _shell != null:
@@ -32,9 +32,20 @@ var _shell: Shell = null:
 
 var on_ice : bool = false
 var in_mud : bool = false
+var _was_on_floor: bool = false
+
+enum Action {
+	NONE,
+	DASH,
+	BASH
+}
+
+var _action : Action = Action.NONE
 
 func _ready():
 	_dash_cooldown = $DashCooldown
+	_bash_cooldown = $BashCooldown
+	_bash_area = $BashArea
 	$PickupDetector.area_entered.connect(
 		func(area: Area3D):
 			if area is ShellPickupArea:
@@ -42,7 +53,6 @@ func _ready():
 	)
 
 	$Model/AnimationPlayer.get_animation("ArmatureAction").loop_mode = Animation.LOOP_LINEAR
-
 
 func set_input(index):
 	left_input = "p" + str(index) + "_left"
@@ -62,15 +72,31 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_just_pressed(action_input) and _dash_cooldown.use_cooldown():
-		_is_dashing = true
-		_dash_direction = -(
-			Vector3(cos(rotation.y + PI), 0., sin(rotation.y)).normalized()
-		)
+	if Input.is_action_just_pressed(action_input):
+		var action = Shell.get_action(_shell)
+		if action == Action.DASH and _dash_cooldown.use_cooldown():
+			_action = Action.DASH
+			_action_direction = -(
+				Vector3(cos(rotation.y + PI), 0., sin(rotation.y)).normalized()
+			)
+			get_tree().create_timer(DASH_TIME, true, true).timeout.connect(func(): _action = Action.NONE)
+		elif action == Action.BASH and _bash_cooldown.use_cooldown():
+			_action = Action.BASH
+			_action_direction = -(
+				Vector3(cos(rotation.y + PI), 0., sin(rotation.y)).normalized()
+			) * 0.3
+			velocity.y = 30
+			gravity = gravity * 15
+			# Needed in case turtle gets stuck on wall
+			get_tree().create_timer(BASH_TIME, true, true).timeout.connect(
+				func():
+					if _action == Action.NONE:
+						return
+					_action = Action.NONE
+					gravity = gravity / 15
+			)
 
-		get_tree().create_timer(DASH_TIME, true, true).timeout.connect(func(): _is_dashing = false)
-
-	if _is_dashing:
+	if _action == Action.DASH or _action == Action.BASH:
 		var factor = 1.0
 
 		if on_ice:
@@ -78,12 +104,23 @@ func _physics_process(delta):
 		elif in_mud:
 			factor /= 1.5
 
-		velocity.x = _dash_direction.x * DASH_SPEED * factor
-		velocity.z = _dash_direction.z * DASH_SPEED * factor
+		var dash_speed = Shell.get_dash_speed(_shell)
+		velocity.x = _action_direction.x * dash_speed * factor
+		velocity.z = _action_direction.z * dash_speed * factor
 		
 		var animator = $Model/AnimationPlayer
 		if animator.is_playing():
 			animator.pause()
+		
+		if !_was_on_floor and is_on_floor() and _action == Action.BASH:
+			_action = Action.NONE
+			gravity = gravity / 15
+
+			for body in _bash_area.get_overlapping_bodies():
+				if body is Turtle and body != self:
+					body.turtle_collision(15, body.position - position)
+				if body is ShellPickup:
+					body.apply_force(50 * body.position - position)
 
 	else:
 		var input_dir = Input.get_vector(left_input, right_input, up_input, down_input)
@@ -96,9 +133,9 @@ func _physics_process(delta):
 				rotation.y = angle
 			else:
 				rotation.y += MAX_ROTATION * sign(diff)
-
-			velocity.x += input_dir.x * ACCELERATION
-			velocity.z += input_dir.y * ACCELERATION
+			var acc = Shell.get_acceleration(_shell)
+			velocity.x += input_dir.x * acc
+			velocity.z += input_dir.y * acc
 
 		var factor = velocity.x * velocity.x + velocity.z * velocity.z
 		var old_y = velocity.y
@@ -132,15 +169,15 @@ func _physics_process(delta):
 		for i in collision_info.get_collision_count():
 			var collider = collision_info.get_collider(i)
 			if collider is Turtle:
-				var power = Shell.get_power(_shell, _is_dashing)
+				var power = Shell.get_power(_shell, _action == Action.DASH)
 				collider.turtle_collision(power, -collision_info.get_normal(i))
 			elif collider is ShellPickup:
 				collider.apply_force(10 * velocity.length() * -collision_info.get_normal(i))
-			elif abs(collision_info.get_normal(i).y) < 0.1 and velocity.length_squared() > 100:
-				velocity = -velocity * 0.8
-				_dash_direction = -_dash_direction
-				rotate_y(PI)
-	
+			#elif abs(collision_info.get_normal(i).y) < 0.1 and velocity.length_squared() > 200:
+			#	velocity = -velocity * 0.8
+			#	_dash_direction = -_dash_direction
+			#	rotate_y(PI)
+	_was_on_floor = is_on_floor()
 	move_and_slide()
 
 func pick_up(shell_type: Shell.Kind):
